fb45e96f0333fb2a5a6613a38e76318e41758514
Luca Bonato <lbonato@magellan.trilan>
2016-10-05 15:58:04 +0200

    Refactored SystemTreeNode and added support for file-system scanning.

--- backup-excluder.py
+++ backup-excluder.py
@@ -2,74 +2,89 @@
 # -*- coding: utf-8 -*-
 
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTreeWidget, QTreeWidgetItem, QVBoxLayout, QHBoxLayout, QLineEdit, QPushButton, QWidget, QPlainTextEdit
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTreeWidget, QTreeWidgetItem, QVBoxLayout, QHBoxLayout, QPushButton, QWidget, QPlainTextEdit
 from model import SystemTreeNode
+from scripts.dirsize import humanize_bytes
+
 
 class ExampleItem(QTreeWidgetItem):
-	def __init__(self, parent, data):
-		super().__init__(parent)
-		self.setText(0, data.name)
-		self.setText(1, str(data.size))
-		
+    def __init__(self, parent, data):
+        super().__init__(parent)
+        self.setText(0, data.name)
+        self.setText(1, humanize_bytes(data.size))
+
+    @staticmethod
+    def fromSystemTree(parent, data):
+        root = ExampleItem(parent, data)
+        for node in sorted(data.children):
+            ExampleItem.fromSystemTree(root, data[node])
+        return root
+
 
 class Example(QMainWindow):
 
-	def __init__(self):
-		super().__init__()
-		self.customInit()
-		
-	def customInit(self):
-		self.tree = QTreeWidget()
-		self.tree.setColumnCount(2)
-		self.root = SystemTreeNode.createStubTree()
-		self.initStubTree()
-		
-		v1 = QVBoxLayout()
-		v1.addWidget(self.tree)
-		
-		self.edit = QPlainTextEdit()
-		self.confirm = QPushButton("apply filters")
-		self.confirm.clicked.connect(self.dosomething)
-		v2 = QVBoxLayout()
-		v2.addWidget(self.confirm)
-		v2.addWidget(self.edit)
-		v2.addStretch(1)
-		
-		h1 = QHBoxLayout()
-		h1.addLayout(v1)
-		h1.addLayout(v2)
-		
-		self.statusBar()
-		tempWidget = QWidget()
-		tempWidget.setLayout(h1)
-		self.setCentralWidget(tempWidget)
-		self.setGeometry(10, 100, 800, 600)
-		self.show()
-		self.dosomething(None)
+    def __init__(self, basePath):
+        super().__init__()
+        self.customInit(basePath)
 
-	def initStubTree(self):
-		n1 = ExampleItem(self.tree, self.root)
-		n2 = ExampleItem(n1, self.root.children[0])
-		n3 = ExampleItem(n2, self.root.children[0].children[0])
-		n4 = ExampleItem(n1, self.root.children[1])
-		n5 = ExampleItem(n2, self.root.children[0].children[1])
-		n6 = ExampleItem(n5, self.root.children[0].children[1].children[0])
+    def customInit(self, basePath):
+        self.tree = QTreeWidget()
+        self.tree.setColumnCount(2)
+        self.tree.setHeaderLabels(["File System", "Size"])
+        self.tree.header().resizeSection(0, 250)
+        self.basePath, self.root = SystemTreeNode.createSystemTree(basePath)
+        ExampleItem.fromSystemTree(self.tree, self.root)
 
-	def dosomething(self, sender):
-		filters = self.edit.document().toPlainText().split("\n")
-		if filters == ['']:
-			filters = []
-		self.statusBar().showMessage("Total sum: " + str(self.root.update("", filters)))
+        v1 = QVBoxLayout()
+        v1.addWidget(self.tree)
+
+        self.edit = QPlainTextEdit()
+        self.confirm = QPushButton("apply filters")
+        self.confirm.clicked.connect(self.applyFilters)
+        v2 = QVBoxLayout()
+        v2.addWidget(self.confirm)
+        v2.addWidget(self.edit)
+        v2.addStretch(1)
+
+        h1 = QHBoxLayout()
+        h1.addLayout(v1)
+        h1.addLayout(v2)
+
+        self.statusBar()
+        tempWidget = QWidget()
+        tempWidget.setLayout(h1)
+        self.setCentralWidget(tempWidget)
+        self.setGeometry(10, 100, 800, 600)
+        self.show()
+        self.applyFilters(None)
+
+    def initStubTree(self):
+        n1 = ExampleItem(self.tree, self.root)
+        n2 = ExampleItem(n1, self.root.children[0])
+        n3 = ExampleItem(n2, self.root.children[0].children[0])
+        n4 = ExampleItem(n1, self.root.children[1])
+        n5 = ExampleItem(n2, self.root.children[0].children[1])
+        n6 = ExampleItem(n5, self.root.children[0].children[1].children[0])
+
+    def applyFilters(self, sender):
+        filters = [x.strip() for x in self.edit.document().toPlainText().split("\n") if x.strip()]
+        finalSize = self.root.update(self.basePath, filters)
+        self.statusBar().showMessage("Total sum: " + humanize_bytes(finalSize))
+
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser(description='backup excluder')
+    parser.add_argument('start', nargs='?', default='.')
+    args = parser.parse_args()
+
+    app = QApplication(sys.argv)
+    window = Example(args.start)
+    retVal = app.exec_()
+    del window
+    del app
+    sys.exit(retVal)
 
 
 if __name__ == '__main__':
-
-	app = QApplication(sys.argv)
-	w = Example()
-	retVal = app.exec_()
-	del w
-	del app
-	sys.exit(retVal)
-	
-		
-	
+    main()
--- model.py
+++ model.py
@@ -1,47 +1,114 @@
 import re
+import os
+
+
+def removePrefix(text, prefix):
+    if text.startswith(prefix):
+        return text[len(prefix):]
+    return text
+
 
 class SystemTreeNode:
 
-	name = None
-	size = None
-	children = None
-	
-	def __init__(self, name, size):
-		self.name = name
-		self.size = size
-		self.children = []
+    # self.name is redoundant since it is the key inside parent.children
+    name = None
+    size = None
+    parent = None
+    children = None
 
-	def matchFilter(self, fullPath, filters):
-		for toExclude in filters:
-			if re.match(toExclude, fullPath):
-				return True
-		return False
-	
-	def update(self, parentPath, filters):
-		fullPath = parentPath + "/" + self.name
-		# assuming that filters can be removed, we must check even for
-		# already excluded elemnts.
-		if self.matchFilter(fullPath, filters): 
-			return 0
-		elif self.children :
-			childrenSum = 0
-			for child in self.children:
-				childrenSum += child.update(fullPath, filters)
-			return self.size + childrenSum
-		
-		return self.size
-		
-	@staticmethod
-	def createStubTree():
-		n1 = SystemTreeNode("N1", 1)
-		n2 = SystemTreeNode("N2", 2)
-		n3 = SystemTreeNode("N3", 3)
-		n4 = SystemTreeNode("N4", 4)
-		n5 = SystemTreeNode("N5", 5)
-		n6 = SystemTreeNode("N6", 6)
-		n1.children += [n2, n4]
-		n2.children += [n3, n5]
-		n5.children += [n6]
-		return n1
+    def __init__(self, name, size=0, parent=None, children=None):
+        self.name = name
+        self.size = size
+        self.parent = parent
+        if children:
+            self.children = children
+        else:
+            self.children = {}
+        for child in self.children.values():
+            child.parent = self
 
-	
+    def __getitem__(self, key):
+        return self.children[key]
+
+    def __setitem__(self, key, value):
+        self.children[key] = value
+        if isinstance(value, SystemTreeNode):
+            value.parent = self
+
+    def matchFilter(self, fullPath, filters):
+        for toExclude in filters:
+            if re.match(toExclude, fullPath):
+                return True
+        return False
+
+    def update(self, parentPath, filters):
+        fullPath = os.path.join(parentPath, self.name)
+        if self.matchFilter(fullPath, filters):
+            return 0
+        elif self.children:
+            childrenSum = 0
+            for child in self.children.values():
+                childrenSum += child.update(fullPath, filters)
+            return self.size + childrenSum
+        return self.size
+
+    def navigateToNode(self, path):
+        head = path
+        folders = []
+        current = self
+        while head:
+            if head == os.sep:
+                break
+            head, tail = os.path.split(head)
+            folders.insert(0, tail)
+        for child in folders:
+            current = current[child]
+        return current
+
+    @staticmethod
+    def createStubTree():
+        '''n1 = SystemTreeNode("N1", 1)
+        n2 = SystemTreeNode("N2", 2)
+        n3 = SystemTreeNode("N3", 3)
+        n4 = SystemTreeNode("N4", 4)
+        n5 = SystemTreeNode("N5", 5)
+        n6 = SystemTreeNode("N6", 6)
+        n1.children += [n2, n4]
+        n2.children += [n3, n5]
+        n5.children += [n6]'''
+        root = SystemTreeNode("1", 1)
+        root["2"] = SystemTreeNode("2", 2)
+        root["2"]["3"] = SystemTreeNode("3", 3)
+        root["2"]["5"] = SystemTreeNode("5", 5)
+        root["2"]["5"]["6"] = SystemTreeNode("6", 6)
+        root["4"] = SystemTreeNode("4", 4)
+        return root
+
+    @staticmethod
+    def createSystemTree(rootFolder="."):
+        import os
+        rootPath = os.path.abspath(rootFolder)
+        head, tail = os.path.split(rootPath)
+        rootNode = SystemTreeNode(tail)
+        for root, dirs, files, rootfd in os.fwalk(rootPath):
+            partialPath = removePrefix(root, rootPath)
+            current = rootNode.navigateToNode(partialPath)
+            for folder in dirs:
+                current[folder] = SystemTreeNode(folder)
+            for file in files:
+                try:
+                    current[file] = SystemTreeNode(file, os.stat(file, dir_fd=rootfd).st_size)
+                except EnvironmentError as err:
+                    print("WARNING: {} in {}".format(err, root))
+        return (head, rootNode)
+
+    def printSubtree(self):
+        current = self
+        result = ""
+        while current.parent:
+            result += ">"
+            current = current.parent
+        result += "(" + str(self.name) + ")"
+        print(result)
+        for node in self.children.values():
+            node.printSubtree()
