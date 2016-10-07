6570dc59df88a1f969eb091cfa5ef80fe21d07b9
Luca Bonato <lbonato@develer.com>
2016-10-07 15:21:09 +0200

    Apply PEP 8 formatting.

--- test_model.py
+++ test_model.py
@@ -9,7 +9,7 @@ from model import SystemTreeNode
 class TestSystemTreeNode(unittest.TestCase):
 
     def setUp(self):
-        self.root =  SystemTreeNode("1", 1, children={
+        self.root = SystemTreeNode("1", 1, children={
             "2": SystemTreeNode("2", 2, children={
                 "3": SystemTreeNode("3", 3),
                 "5": SystemTreeNode("5", 5, children={
@@ -27,7 +27,8 @@ class TestSystemTreeNode(unittest.TestCase):
         self.assertEqual(aNode.parent, self.root)
         self.assertTrue(aNode.name in self.root.children)
         self.assertEqual(self.root.children[aNode.name], aNode)
-        self.assertRaises(Exception, self.root.addChild, {'name': 'bNode', 'size': 1})
+        fakeNode = {'name': 'bNode', 'size': 1}
+        self.assertRaises(Exception, self.root.addChild, fakeNode)
         del aNode
 
     def test_navigateToNode_existing_node(self):
@@ -41,8 +42,10 @@ class TestSystemTreeNode(unittest.TestCase):
         self.assertEqual(n2.navigateToNode("5"), n5)
 
     def test_navigateToNode_nonexisting_node(self):
-        self.assertRaises(KeyError, self.root.navigateToNode, "bad/node/from/start")
-        self.assertRaises(KeyError, self.root.navigateToNode, "2/3/bad/node")
+        self.assertRaises(KeyError, self.root.navigateToNode,
+                          "bad/node/from/start")
+        self.assertRaises(KeyError, self.root.navigateToNode,
+                          "2/3/bad/node")
 
     def test_navigateToNode_bad_path(self):
         self.assertRaises(KeyError, self.root.navigateToNode, "/1/2/3")
@@ -50,33 +53,34 @@ class TestSystemTreeNode(unittest.TestCase):
         n5 = self.root.getChild("2").getChild("5")
         self.assertRaises(KeyError, n5.navigateToNode, "/2/3")
 
-
     def test_update_mock_regex(self):
-        #ignore cutFunction
+        # ignore cutFunction
         fullsize = self.root.update("", lambda x: False)
         self.assertEqual(fullsize, 21)
         nosize = self.root.update("", lambda x: True)
         self.assertEqual(nosize, 0)
 
-    def _perform_update_with_regex(self, node, parentPath, regex, expectedResult):
+    def _perform_update_with_regex(self, node, parentPath,
+                                   regex, expectedResult):
         result = node.update(parentPath, re.compile(regex).match)
         self.assertEqual(result, expectedResult)
 
     def test_update_valid_regex(self):
-        #supply cutFunction matching a leaf node
+        # supply cutFunction matching a leaf node
         expectedResult = 21 - 6
-        self._perform_update_with_regex(self.root, "", "1/2/5/6", expectedResult)
-        #supply cutFunction matching a inner node
+        self._perform_update_with_regex(self.root, "", "1/2/5/6",
+                                        expectedResult)
+        # supply cutFunction matching a inner node
         expectedResult = 21 - (2 + 3 + 5 + 6)
         self._perform_update_with_regex(self.root, "", "1/2", expectedResult)
 
     def test_update_parent_path(self):
-        #supply an invalid parentPath
-        self._perform_update_with_regex(self.root, "invalid/parent/path", "1/2", 21)
-        #supply a valid parentPath
+        # supply an invalid parentPath
+        self._perform_update_with_regex(self.root, "invalid/parent/path",
+                                        "1/2", 21)
+        # supply a valid parentPath
         startingNode = self.root.getChild("2").getChild("5")
         self._perform_update_with_regex(startingNode, "1/2", "1/2/5/6", 5)
-
 
 
 if __name__ == '__main__':
